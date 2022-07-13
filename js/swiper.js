5d50da6aa8fde4d17c6f52df863021215f59d0c5
Sans0614 <rebound19960614@gmail.com>
2022-07-13 16:55:16 +0800

    20220713-01 update

---
+++ js/swiper.js
@@ -0,0 +1,119 @@
+var swiper = new Swiper(".service-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".service-swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".service-swiper-button-next",
+    prevEl: ".service-swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
+
+
+var swiper = new Swiper(".industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".food-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".food-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".health-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".health-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".medicine-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".medicine-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".skincare-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".skincare-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".apparel-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".apparel-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".social-industry-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".social-industry-swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".service-process-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".service-process-swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".service-process-button-next",
+    prevEl: ".service-process-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".our-team-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".our-team-swiper-pagination",
+    clickable: true,
+  },
+});

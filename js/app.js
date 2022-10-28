505202be9b4a88c1fd44d74c92b0666da293a9d9
Sans <rebound19960614@gmail.com>
2022-10-28 18:46:07 +0800

    20221028-01 index update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   //要執行回到最上方
-  $('#to-top-button').on('click',function(){
+  $('#goto-top-fixed-button').on('click',function(){
      $("html,body").animate(
       {
         scrollTop:0 //回到第一個區塊
